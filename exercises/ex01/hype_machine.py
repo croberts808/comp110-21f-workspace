63f87539d1954ad3a50a5ccc43b84d1fcc2459a9
Catherine Roberts <crobert7@email.unc.edu>
2021-08-31 11:29:50 -0400

    testing

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,12 @@
+"""This is me testing out the exercise."""
 # TODO: Write docstring here
 
 # TODO: Initialize __author__ variable here
+__author__ = "1234567890"
+
 
 # TODO: Implement your program logic here
+
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
