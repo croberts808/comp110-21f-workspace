e6bee7f227ad6e12cbf712840955e6bd60f03cb1
Catherine Roberts <crobert7@email.unc.edu>
2021-09-13 11:04:46 -0400

    initial commit

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,18 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730439833"
+
+word: str = input("Enter a word: ")
+i: int = 0
+tf: bool = False
+
+while i < len(word):
+    character: str = word[i]
+    index: int = i + 1
+    while index < len(word):
+        if character == word[index]:
+            tf = True
+        index += 1
+    i += 1
+
+print("Found Duplicate: " + str(tf))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,15 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730439833"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+depth: int = int(input("Enter a number: "))
+i: int = 0
+
+if depth == 0:
+    print("Depth: 0")
+else:
+    while i < depth:
+        print(TREE)
+        i += 1
