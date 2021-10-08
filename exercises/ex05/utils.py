8f386191b00937786bfdc85bf13a783236065f61
Catherine Roberts <crobert7@email.unc.edu>
2021-10-08 15:08:00 -0400

    initial

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -16,20 +16,25 @@ def only_evens(a: list[int]) -> list[int]:
     return b
 
 
-def sub(a: list[int], b: int, c: int) -> list[int]:
-    empty: lst[int] = []
-    if len(a) == 0 or b > len(a) or c <= 0:
+def sub(a: list[int], start: int, end: int) -> list[int]:
+    """Function that returns a subset of the list we gave it."""
+    empty: list[int] = []
+    if len(a) == 0 or start > len(a) or end <= 0:
         return empty
-
-    i: int = b
+    if start < 0:
+        start = 0
+    if end > len(a):
+        end = len(a)
+    i: int = start
     sub: list[int] = []
-    while b < c:
+    while i < end:
         sub.append(a[i])
         i += 1
     return sub
 
 
 def concat(a: list[int], b: list[int]) -> list[int]:
+    """A function that concatenates two lists."""
     new: list[int] = []
     i: int = 0
     j: int = 0
@@ -37,6 +42,6 @@ def concat(a: list[int], b: list[int]) -> list[int]:
         new.append(a[i])
         i += 1
     while j < len(b):
-        new.append(b[i])
+        new.append(b[j])
         j += 1
     return new
